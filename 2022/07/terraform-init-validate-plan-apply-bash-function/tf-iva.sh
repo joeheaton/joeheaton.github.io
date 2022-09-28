35d0464f5e3e963f38ae61e7710a1fb0d64d63fc
joeheaton <joeheaton@users.noreply.github.com>
2022-09-28 10:59:04 +0000

    deploy: joeheaton/hugs@35b3afd9bcc1607ad3e9189a6b4cdb638d6f8eed

--- 2022/07/terraform-init-validate-plan-apply-bash-function/tf-iva.sh
+++ 2022/07/terraform-init-validate-plan-apply-bash-function/tf-iva.sh
@@ -1,18 +1,46 @@
 # tf-iva - init, validate, plan, apply
 function tf-iva {
-    if [[ "${1}" =~ ^(-h|--help)$ ]]; then
+    if [[ "${1}" =~ ^(-h|--?help)$ ]]; then
 		cat <<-EOT
 			tf-iva - Terraform init, validate, plan, apply by Joe Heaton.dev
 
-			Usage: [TF_PLANS=DIR] tf-iva [chdir]
+			Usage: [TF_PLANS=DIR] tf-iva [FLAGS] [chdir]
 
-		Default destiantion for tfplans is \$TMPDIR/terraform
+			Flags:
+			    -h|--help    - Display this help text
+			    -d|--destroy - Perform a destroy instead of apply
+			    -t|--target  - Targetted apply (does not support destroy)
+
+			Example:
+			    tf-iva --target module.folder my_terraform
+
+			Default destiantion for tfplans is \$TMPDIR/terraform
 		EOT
         return
     fi
 
-    # Use argument as dir
-    [ "${1}" ] && DIR="${1}" || DIR=""
+    terraform="$( which terraform 2>/dev/null )"
+
+    # --destroy and exit
+    if [[ "${1}" =~ ^(-d|--?destroy)$ ]]; then
+        DIR="${2}"
+        echo
+        echo "> terraform ${DIR:+-chdir=${DIR}} destroy"
+        $terraform ${DIR:+-chdir=${DIR}} destroy
+        return
+    fi
+
+    # --target set TARGET & DIR
+    if [[ "${1}" =~ ^(-t|--?target)$ ]]; then
+        TARGET="${2}"
+        DIR="${3}"
+    fi
+
+    # Use argument as DIR if DIR unset
+    [ ! "${DIR}${TARGET}" ] && [ "${1}" ] && DIR="${1}" || DIR=""
+
+    # Abort if DIR unset
+    [ ! "${DIR}${TARGET}" ] && { echo "DIR unset.. Aborting!"; return 1; }
 
     # Set tfplan destination
     if [ "${TF_PLANS}" ]; then
@@ -31,31 +59,30 @@ function tf-iva {
     
     echo
     echo "> terraform ${DIR:+-chdir=${DIR}} init"
-    terraform ${DIR:+-chdir=${DIR}} init
+    $terraform ${DIR:+-chdir=${DIR}} init
     [ ! "$?" = 0 ] && { echo "Error: Terraform init failed."; return 1; }
 
     echo
     echo "> terraform ${DIR:+-chdir=${DIR}} validate"
-    terraform ${DIR:+-chdir=${DIR}} validate
+    $terraform ${DIR:+-chdir=${DIR}} validate
     [ ! "$?" = 0 ] && { echo "Error: Terraform validate failed."; return 1; }
 
-    echo
-    echo "> terraform ${DIR:+-chdir=${DIR}} plan -out ${DEST}/${UUID}.tfplan"
-    terraform ${DIR:+-chdir=${DIR}} plan -out ${DEST}/${UUID}.tfplan
+    echo "> terraform ${DIR:+-chdir=${DIR}} plan ${TARGET:+-target=${TARGET}} -out ${DEST}/${UUID}.tfplan"
+    $terraform ${DIR:+-chdir=${DIR}} plan ${TARGET:+-target=${TARGET}} -out ${DEST}/${UUID}.tfplan
     [ ! "$?" = 0 ] && { echo "Error: Terraform plan failed."; return 1; }
 
-    echo
-    read -n1 -p "Apply this plan? [Y/n]: " yn
+    read -r -p "Apply this plan? [Y/n]: " yn
+	[[ ! "${yn,,}" =~ ^y|yes$ ]] && return 1
     echo
     case $yn in
         [nN]*) echo "Aborting.."; return;;
         [yY]*)
-            echo "> terraform ${DIR:+-chdir=${DIR}} apply ${DEST}/${UUID}.tfplan"
-            terraform ${DIR:+-chdir=${DIR}} apply ${DEST}/${UUID}.tfplan
+            echo "> terraform ${DIR:+-chdir=${DIR}} apply ${TARGET:+-target=${TARGET}} ${DEST}/${UUID}.tfplan"
+            $terraform ${DIR:+-chdir=${DIR}} apply ${TARGET:+-target=${TARGET}} ${DEST}/${UUID}.tfplan
             ;;
         "")
-            echo "> terraform ${DIR:+-chdir=${DIR}} apply ${DEST}/${UUID}.tfplan"
-            terraform ${DIR:+-chdir=${DIR}} apply ${DEST}/${UUID}.tfplan
+            echo "> terraform ${DIR:+-chdir=${DIR}} apply ${TARGET:+-target=${TARGET}} ${DEST}/${UUID}.tfplan"
+            $terraform ${DIR:+-chdir=${DIR}} apply ${TARGET:+-target=${TARGET}} ${DEST}/${UUID}.tfplan
             ;;
         *) echo "Aborting.."; return;;
     esac 
